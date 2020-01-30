24403acbdfea3f16112a683a185344d8eba3e70e
veroj_000 <veroj_000@JEREMY-LAPTOP>
2020-01-30 17:27:18 -0500

    commit errors

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -18,95 +18,103 @@ namespace ConsoleApp1
                 //Converts string to interger
                 int grade = int.Parse(input);
 
-                //If statement to test user's input to grades on syllabus
-                if ((grade <= 100) && (grade >= 98))
+                if ((grade >= 0) && (grade <= 100))
                 {
-                    //Displays the projected grade based on input
-                    Console.WriteLine("Your projected grade for ISM 4300 is an A+!");
+                    //If statement to test user's input to grades on syllabus
+                    if ((grade <= 100) && (grade >= 98))
+                    {
+                        //Displays the projected grade based on input
+                        Console.WriteLine("Your projected grade for ISM 4300 is an A+!");
+                    }
+
+                    //Else if statement to test all the other ossible inputs
+                    else if ((grade <= 97) && (grade >= 92))
+                    {
+                        //Displays the projected grade based on input
+                        Console.WriteLine("Your projected grade for ISM 4300 is an A!");
+                    }
+
+                    //Else if statement to test all the other ossible inputs
+                    else if ((grade <= 91) && (grade >= 90))
+                    {
+                        //Displays the projected grade based on input
+                        Console.WriteLine("Your projected grade for ISM 4300 is an A-!");
+                    }
+
+                    //Else if statement to test all the other ossible inputs
+                    else if ((grade <= 89) && (grade >= 88))
+                    {
+                        //Displays the projected grade based on input
+                        Console.WriteLine("Your projected grade for ISM 4300 is an B+!");
+                    }
+
+                    //Else if statement to test all the other ossible inputs
+                    else if ((grade <= 87) && (grade >= 82))
+                    {
+                        //Displays the projected grade based on input
+                        Console.WriteLine("Your projected grade for ISM 4300 is an B!");
+                    }
+
+                    //Else if statement to test all the other ossible inputs
+                    else if ((grade <= 81) && (grade >= 80))
+                    {
+                        //Displays the projected grade based on input
+                        Console.WriteLine("Your projected grade for ISM 4300 is an B-!");
+                    }
+
+                    //Else if statement to test all the other ossible inputs
+                    else if ((grade <= 79) && (grade >= 78))
+                    {
+                        //Displays the projected grade based on input
+                        Console.WriteLine("Your projected grade for ISM 4300 is an C+!");
+                    }
+
+                    //Else if statement to test all the other ossible inputs
+                    else if ((grade <= 77) && (grade >= 72))
+                    {
+                        //Displays the projected grade based on input
+                        Console.WriteLine("Your projected grade for ISM 4300 is an C!");
+                    }
+
+                    //Else if statement to test all the other ossible inputs
+                    else if ((grade <= 71) && (grade >= 70))
+                    {
+                        //Displays the projected grade based on input
+                        Console.WriteLine("Your projected grade for ISM 4300 is an C-!");
+                    }
+
+                    //Else if statement to test all the other ossible inputs
+                    else if ((grade <= 69) && (grade >= 68))
+                    {
+                        //Displays the projected grade based on input
+                        Console.WriteLine("Your projected grade for ISM 4300 is an D+!");
+                    }
+
+                    //Else if statement to test all the other ossible inputs
+                    else if ((grade <= 67) && (grade >= 62))
+                    {
+                        //Displays the projected grade based on input
+                        Console.WriteLine("Your projected grade for ISM 4300 is an D!");
+                    }
+
+                    //Else if statement to test all the other ossible inputs
+                    else if ((grade <= 61) && (grade >= 60))
+                    {
+                        //Displays the projected grade based on input
+                        Console.WriteLine("Your projected grade for ISM 4300 is an D-!");
+                    }
+
+                    //Else if statement to test all the other ossible inputs
+                    else if (grade < 60)
+                    {
+                        //Displays the projected grade based on input
+                        Console.WriteLine("Your projected grade for ISM 4300 is an F!");
+                    }
                 }
 
-                //Else if statement to test all the other ossible inputs
-                else if ((grade <= 97) && (grade >= 92))
+                else
                 {
-                    //Displays the projected grade based on input
-                    Console.WriteLine("Your projected grade for ISM 4300 is an A!");
-                }
-
-                //Else if statement to test all the other ossible inputs
-                else if ((grade <= 91) && (grade >= 90))
-                {
-                    //Displays the projected grade based on input
-                    Console.WriteLine("Your projected grade for ISM 4300 is an A-!");
-                }
-
-                //Else if statement to test all the other ossible inputs
-                else if ((grade <= 89) && (grade >= 88))
-                {
-                    //Displays the projected grade based on input
-                    Console.WriteLine("Your projected grade for ISM 4300 is an B+!");
-                }
-
-                //Else if statement to test all the other ossible inputs
-                else if ((grade <= 87) && (grade >= 82))
-                {
-                    //Displays the projected grade based on input
-                    Console.WriteLine("Your projected grade for ISM 4300 is an B!");
-                }
-
-                //Else if statement to test all the other ossible inputs
-                else if ((grade <= 81) && (grade >= 80))
-                {
-                    //Displays the projected grade based on input
-                    Console.WriteLine("Your projected grade for ISM 4300 is an B-!");
-                }
-
-                //Else if statement to test all the other ossible inputs
-                else if ((grade <= 79) && (grade >= 78))
-                {
-                    //Displays the projected grade based on input
-                    Console.WriteLine("Your projected grade for ISM 4300 is an C+!");
-                }
-
-                //Else if statement to test all the other ossible inputs
-                else if ((grade <= 77) && (grade >= 72))
-                {
-                    //Displays the projected grade based on input
-                    Console.WriteLine("Your projected grade for ISM 4300 is an C!");
-                }
-
-                //Else if statement to test all the other ossible inputs
-                else if ((grade <= 71) && (grade >= 70))
-                {
-                    //Displays the projected grade based on input
-                    Console.WriteLine("Your projected grade for ISM 4300 is an C-!");
-                }
-
-                //Else if statement to test all the other ossible inputs
-                else if ((grade <= 69) && (grade >= 68))
-                {
-                    //Displays the projected grade based on input
-                    Console.WriteLine("Your projected grade for ISM 4300 is an D+!");
-                }
-
-                //Else if statement to test all the other ossible inputs
-                else if ((grade <= 67) && (grade >= 62))
-                {
-                    //Displays the projected grade based on input
-                    Console.WriteLine("Your projected grade for ISM 4300 is an D!");
-                }
-
-                //Else if statement to test all the other ossible inputs
-                else if ((grade <= 61) && (grade >= 60))
-                {
-                    //Displays the projected grade based on input
-                    Console.WriteLine("Your projected grade for ISM 4300 is an D-!");
-                }
-
-                //Else if statement to test all the other ossible inputs
-                else if (grade < 60)
-                {
-                    //Displays the projected grade based on input
-                    Console.WriteLine("Your projected grade for ISM 4300 is an F!");
+                    Console.WriteLine("Please insert an interger value between 1 and 100");
                 }
             } //end of try
 
